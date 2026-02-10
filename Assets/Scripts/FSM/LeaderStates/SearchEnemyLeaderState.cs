b1320733c0b427a7126aaf49b5189eb01618d3b2
Nico <nicolas.migues97@gmail.com>
2026-02-10 11:59:57 -0300

    PRELEADERATTACK

---
+++ Assets/Scripts/FSM/LeaderStates/SearchEnemyLeaderState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchEnemyLeaderState : IState
+{
+    public void OnEnter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnExit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
