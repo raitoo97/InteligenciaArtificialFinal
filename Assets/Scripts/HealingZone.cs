3910bdf0063bd5c8ae57880d28c01b57ddcb8588
Nico <nicolas.migues97@gmail.com>
2026-02-11 22:57:28 -0300

    PREFINALTEST

--- Assets/Scripts/HealingZone.cs
+++ Assets/Scripts/HealingZone.cs
@@ -19,12 +19,29 @@ public class HealingZone : MonoBehaviour
                 boid.Life.Heal(healAmount);
             }
         }
+        if (other.gameObject.layer == 10)
+        {
+            var leader = other.GetComponent<Leader>();
+            if (leader == null) return;
+            if (IsCorrectLeaderTeam(leader))
+            {
+                leader.Life.Heal(healAmount);
+            }
+        }
     }
     private bool IsCorrectTeam(TypeBoid boidType)
     {
         if (zoneType == HealingZoneTeam.Blue && boidType == TypeBoid.BlueTeam)
             return true;
         if (zoneType == HealingZoneTeam.Violet && boidType == TypeBoid.VioletTeam)
+            return true;
+        return false;
+    }
+    private bool IsCorrectLeaderTeam(Leader leaderType)
+    {
+        if(zoneType == HealingZoneTeam.Violet && leaderType.IsVioletLeader)
+            return true;
+        if(zoneType == HealingZoneTeam.Blue && !leaderType.IsVioletLeader)
             return true;
         return false;
     }
